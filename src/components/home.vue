a344f0d19674436476d2ec97ce105f3c03b570dc
jiyun0204 <bomchoon0204@gmail.com>
2021-06-18 10:08:21 +0900

    init

---
+++ src/components/home.vue
@@ -0,0 +1,40 @@
+<template>
+  <h2>home</h2>
+  <div>
+     <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="#">Navbar</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Features</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Pricing</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link disabled" href="#">Disabled</a>
+      </li>
+    </ul>
+  </div>
+</nav>
+  </div>
+</template>
+
+<script>
+export default {
+    neme:"header"
+
+
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/pdata.js
@@ -0,0 +1,70 @@
+export default [{
+        id : 0,
+        title: "버킷햇",
+        image: "image/hmbkt.jpg",
+        content: "코튼 혼방 소재의 버킷햇. 내부에 스웨트밴드 부착",
+        price:14900
+},
+{
+    id : 1,
+    title: "숄더백",
+    image: "image/hmbag.jpg",
+    content: "인조 가죽 소재의 소형 숄더백",
+    price:29900
+},
+{
+    id : 2,
+    title: "퀼티드 미니 백",
+    image: "image/hmbagbagi.jpg",
+    content: "얇게 패딩된 인조 가죽 소재의 미니 퀼팅 백",
+    price:19900
+},
+{
+    id : 3,
+    title: "포인티드 펌프스",
+    image: "image/hmshs0.jpg",
+    content: "인조 가죽 소재의 포인티드 토 펌프스",
+    price:34900
+},
+{
+    id : 4,
+    title: "플랫폼 슬리퍼 샌들",
+    image: "image/hmshs1.jpg",
+    content: "밑창 가장자리가 주트 소재로 마감됨",
+    price:49900
+},
+{
+    id : 5,
+    title: "링 이어링 6쌍 세트",
+    image: "image/hmacc.jpg",
+    content: "다양한 텍스처의 금속 링 귀걸이 세트",
+    price:17900
+},
+{
+    id : 6,
+    title: "선글라스",
+    image: "image/hmaccsun.jpg",
+    content: "캣아이 스타일의 플라스틱 프레임 선글라스",
+    price:14900
+},
+{
+    id : 7,
+    title: "플랫폼 에스파드리유",
+    image: "image/hmshs2.jpg",
+    content: "리넨을 함유한 코튼 캔버스 소재의 플랫폼 에스파드리유",
+    price:39900
+},
+{
+    id : 8,
+    title: "브레이디드 벨트",
+    image: "image/hmbt.jpg",
+    content: "인조 가죽 소재의 와이드한 위빙 벨트",
+    price:29900
+},
+{
+    id : 9,
+    title: "매듭 헤어밴드",
+    image: "image/hmhbd.jpg",
+    content: "상단에 매듭 장식이 있는 인조 가죽 소재의 헤어밴드",
+    price:14900
+}]
